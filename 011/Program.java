6fb8fc305bf42070d43b2d312010b5bbf9091283
TeemuViikeri <teemu.viikeri@tuni.fi>
2021-01-18 11:10:21 +0200

    Remove old file due to renaming.

--- 011/Program.java
+++
@@ -1,8 +0,0 @@
-public class Program {
-    public static void main(String[] args) {
-        Shape shaper = new Shape();
-        int height = 5;
-
-        shaper.fallingLines(height, "*");
-    }
-}
