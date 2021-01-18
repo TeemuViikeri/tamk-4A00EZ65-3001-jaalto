63f01e4ac40df5d5335013c4030c16bd9c8d7e65
TeemuViikeri <teemu.viikeri@tuni.fi>
2021-01-18 11:04:55 +0200

    Change file name.

---
+++ 011/ShapeLineFallingRight.java
@@ -0,0 +1,8 @@
+public class ShapeLineFallingRight {
+    public static void main(String[] args) {
+        Shape shaper = new Shape();
+        int height = 5;
+
+        shaper.fallingLines(height, "*");
+    }
+}
